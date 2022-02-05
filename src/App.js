047c8de4cb9c9e5fff120c421e45c2c01bf219f1
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-05 02:55:53 +0100

    added main page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import "./App.css";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { actionsPosts } from "./redux/posts/actions"
+import "./css/style.css";
+import MainPage from "./components/mainPage/mainPage";
 
 
 function App() {
-
-  const dispatch = useDispatch();
-  const data = useSelector(state => state.posts)
-
-  useEffect(() => {
-    dispatch(actionsPosts())
-  }, [dispatch]);
-
-  console.log(data)
-
-  return <p>Hello</p>;
+  return <MainPage />
 }
 
 export default App;
