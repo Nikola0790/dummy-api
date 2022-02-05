61e7e581d6418db6c7e92a1596ba7ff9b21c5b9c
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-05 02:53:31 +0100

    added error

--- src/redux/posts/reducer.jsx
+++ src/redux/posts/reducer.jsx
@@ -1,30 +1,33 @@
 import { POSTS_SUCCESS, POSTS_REQUEST, POSTS_FAIL } from "./types";
 
 const initialState = {
-    loading: false,
-    data: [],
+  loading: true,
+  data: [],
+  error: "",
 };
 
 const reducerPosts = (state = initialState, action) => {
-    switch (action.type) {
-        case POSTS_REQUEST:
-            return {
-                ...state,
-                loading: true,
-            };
-        case POSTS_SUCCESS: 
-            return {
-                loading: false,
-                data: action.payload,
-            };
-        case POSTS_FAIL:
-            return {
-                loading: false,
-                data: action.payload,
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case POSTS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case POSTS_SUCCESS:
+      return {
+        loading: false,
+        data: action.payload,
+        error: "",
+      };
+    case POSTS_FAIL:
+      return {
+        loading: false,
+        data: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducerPosts;
