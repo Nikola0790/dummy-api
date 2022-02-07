46d0e94e92c9a5913cbaf1e78adeadde449f5515
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-07 17:17:07 +0100

    added reducerEditPost

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import reducerPosts from "./posts/reducer";
 import reducerSinglePostDetails from "./singlePost/reducer";
+import reducerEditPost from "./editPost/reducer";
 
 const rootReducer = combineReducers({
-    posts: reducerPosts,
-    singlePost: reducerSinglePostDetails,
-})
+  posts: reducerPosts,
+  singlePost: reducerSinglePostDetails,
+  editedPost: reducerEditPost,
+});
 
 export default rootReducer;
